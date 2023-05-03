ef52eea599092827c0c601651b371c254e2dbf34
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-05-02 22:33:42 -0600

    Metodos

---
+++ TestHero/Models/AlumnoExamen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class AlumnoExamen
+{
+    public int IdAlumno { get; set; };
+    public int IdExamen { get; set; };
+    public int calificacion { get; set; };
+    public int puntos { get; set; };
+    public string fechaRealizacion { get; set; };
+
+}
