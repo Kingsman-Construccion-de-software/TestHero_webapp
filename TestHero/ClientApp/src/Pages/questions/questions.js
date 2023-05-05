208a6917abfd9cd1da1c5e9b1933b90081445248
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-05-04 22:32:52 -0600

    Avance

--- TestHero/ClientApp/src/Pages/questions/questions.js
+++ TestHero/ClientApp/src/Pages/questions/questions.js
@@ -1,27 +1,41 @@
-import styles from "./questions.css"
-import Sidebar from "../../components/sidebar/Sidebar"
-import Pregunta from "../../components/pregunta/Pregunta"
-
+import styles from "./questions.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Pregunta from "../../components/pregunta/Pregunta";
+import { useState, useEffect } from "react";
+import axios from "axios";
 export default function Questions() {
-    return (
+  const [preguntas, setPreguntas] = useState([]);
+
+  const URIpreguntas = "api/pregunta/examen/1";
+
+  const getPreguntas = async () => {
+    const res = await axios.get(URIpreguntas);
+    setPreguntas(res.data);
+  };
+
+  useEffect(() => {
+    getPreguntas();
+  }, []);
+
+  return (
+    <div>
       <div>
-        <div>
-            <Sidebar />
+        <Sidebar />
+      </div>
+      <div className="page">
+        <div className="content">
+          <h1 className="tituloExamen">Examen de trigonometría</h1>
+          <div className="subtitles">
+            <h2>Preguntas</h2>
+            <h2>Código: 13467942</h2>
+          </div>
+          <div className="preguntas">
+            {preguntas.map((preguntas, index) => (
+              <Pregunta key={index} preguntas={preguntas} />
+            ))}
+          </div>
         </div>
-        <div className="page">
-            <div className="content">
-                <h1 className="tituloExamen">Examen de trigonometría</h1> 
-                <div className="subtitles">
-                    <h2>Preguntas</h2>
-                    <h2>Código: 13467942</h2>
-                </div> 
-                <div className="preguntas">
-                    <Pregunta/>  
-                    <Pregunta/>  
-                    <Pregunta/>  
-                </div>         
-            </div>
-        </div>
+      </div>
     </div>
-    )
+  );
 }
