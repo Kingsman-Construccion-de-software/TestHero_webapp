fd393bd66fb953a480423e0fc6e7bc2eae6677a0
LGnan <leogarcia2003@hotmail.com>
2023-05-24 17:04:33 -0600

    Resolución de conflictos en rutas + diseño cambiado en sidebarAlumno

--- TestHero/ClientApp/src/components/sidebarAlumno/SidebarAlumno.js
+++ TestHero/ClientApp/src/components/sidebarAlumno/SidebarAlumno.js
@@ -1,10 +1,9 @@
 import React from "react";
 import SidebarLogo from "../../assets/SidebarLogo.png";
 import Home from "../../assets/Home.png";
+import Tasks from "../../assets/Tasks.png";
 import BackArrow from "../../assets/BackArrow.png"
-import Questions from "../../assets/Questions.png";
-import Groups from "../../assets/Groups.png";
-import Analytics from "../../assets/Analytics.png";
+//import Analytics from "../../assets/Analytics.png";
 import Out from "../../assets/Out.png";
 import { Link } from "react-router-dom";
 
@@ -18,26 +17,23 @@ import "./SidebarAlumno.css";
  */
 
 export default function Sidebar() {
-    return (
-        <div className="sidenav">
-            <img className="logoPrincipal" src={SidebarLogo} alt="logo" />
-            <Link to={"/home"}>
-                <img className="iconSidebar" src={BackArrow} alt="GoBack" />
-            </Link>
-            <Link to={"/home"}>
-                <img className="iconSidebar" src={Home} alt="Home" />
-            </Link>
-            <Link to={"/grupos"}>
-                <img className="iconSidebar" src={Groups} alt="Groups" />
-            </Link>
+  return (
+    <div className="sidenav">
+      <img className="logoPrincipal" src={SidebarLogo} alt="logo" />
+      <Link to={"/homealumno"}>
+         <img className="iconSidebar" src={BackArrow} alt="GoBack" />
+      </Link>
 
-            <Link to={"/"}>
-                <img className="iconSidebar" src={Out} alt="Log out" />
-            </Link>
-        </div>
-    );
+      <Link to={"/homealumno"}>
+      <img className="iconSidebar" src={Home} alt="Home" />
+      </Link>
+
+      <Link to={"/examenes"}>
+        <img className="iconSidebar" src={Tasks} alt="Tasks" />
+      </Link>
+      <Link to={"/"}>
+        <img className="iconSidebar" src={Out} alt="Log out" />
+      </Link>
+    </div>
+  );
 }
-// Luego metemos las analiticas,
-/*<Link to={"/grupos"}>
-<img className="iconSidebar" src={Analytics} alt="Analiticas" />
-</Link>*/ 
