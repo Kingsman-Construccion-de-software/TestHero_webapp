bf810df53dc62393c98d327cea582360018586f5
LGnan <leogarcia2003@hotmail.com>
2023-05-24 12:45:00 -0600

    Ya actualice la flecha para atrás en el login como puse en el Sigma

--- TestHero/ClientApp/src/Pages/login/Login.js
+++ TestHero/ClientApp/src/Pages/login/Login.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./login.css";
 import logo from "../../assets/logo.png";
+import BackArrow from "../../assets/BackArrow.png"
 import { useState } from "react";
 import axios from "axios";
 import { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import ProfesorContext from "context/contextoProfesor";
+// import { useHistory } from 'react-router-dom';
+// import PreLogin from "Pages/prelogin/PreLogin";
 
 /**
- * @author Bernardo de la Sierra
+ * @author Bernardo de la Sierra y Leonardo García 
  * @version 2.1.1
  * @license Gp
  * @params Sin parametros
@@ -16,95 +19,101 @@ import ProfesorContext from "context/contextoProfesor";
  */
 
 export default function Login() {
-  // Parametros a actualizar
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [status, setStatus] = useState("");
-  const { state, setState } = useContext(ProfesorContext);
-  const navigate = useNavigate();
-  /**Cambia el correo */
-  const handleEmailChange = (value) => {
-    setEmail(value);
-  };
-  /**Cambia ela contraseña */
-  const handlePasswordChange = (value) => {
-    setPassword(value);
-  };
-  /** Hace el inicar sesion */
-  const handleLogin = async (e) => {
-    e.preventDefault();
+    // Parametros a actualizar
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [status, setStatus] = useState("");
+    const { state, setState } = useContext(ProfesorContext);
+    const navigate = useNavigate();
+    /**Cambia el correo */
+    const handleEmailChange = (value) => {
+        setEmail(value);
+    };
+    /**Cambia ela contraseña */
+    const handlePasswordChange = (value) => {
+        setPassword(value);
+    };
+    /** Hace el inicar sesion */
+    const handleLogin = async (e) => {
+        e.preventDefault();
 
-    const data = {
-      Correo: email,
-      Password: password,
+        const data = {
+            Correo: email,
+            Password: password,
+        };
+
+        const url = "api/login";
+
+        try {
+            const result = await axios.post(url, data);
+            setStatus(result.data.message);
+            if (result.data.message === "Login exitoso") {
+                getProfesor(result.data.id);
+            }
+        } catch (error) {
+            alert(error);
+        }
+    };
+    /**Checa que dado un idProfesor nos guarde su informacion */
+    const getProfesor = async (id) => {
+        const url = `api/profesor/${id}`;
+
+        try {
+            const result = await axios.get(url);
+            let res = result.data[0];
+            let profesor = {
+                id: res.idProfesor,
+                nombre: res.nombre + " " + res.apellido,
+                correo: res.correo,
+            };
+            setState(profesor);
+        } catch (error) {
+            alert(error);
+        }
     };
 
-    const url = "api/login";
+    useEffect(() => {
+        if (status === "Login exitoso") {
+            navigate("/home");
+        }
+    }, [status]);
 
-    try {
-      const result = await axios.post(url, data);
-      setStatus(result.data.message);
-      if (result.data.message === "Login exitoso") {
-        getProfesor(result.data.id);
-      }
-    } catch (error) {
-      alert(error);
-    }
-  };
-  /**Checa que dado un idProfesor nos guarde su informacion */
-  const getProfesor = async (id) => {
-    const url = `api/profesor/${id}`;
+    // función para hacer el boton de regreso
+    function GoBack() {
+        navigate("/");
+    };
 
-    try {
-      const result = await axios.get(url);
-      let res = result.data[0];
-      let profesor = {
-        id: res.idProfesor,
-        nombre: res.nombre + " " + res.apellido,
-        correo: res.correo,
-      };
-      setState(profesor);
-    } catch (error) {
-      alert(error);
-    }
-  };
-
-  useEffect(() => {
-    if (status === "Login exitoso") {
-      navigate("/home");
-    }
-  }, [status]);
-
-  return (
-    <div className="login">
-      <div className="loginWrapper">
-        <img src={logo} alt="Logo testHero" />
-        <form className="loginBox" onSubmit={handleLogin}>
-          <p className="texto">Correo</p>
-          <input
-            placeholder="Correo"
-            type="email"
-            className="loginInput"
-            onChange={(e) => handleEmailChange(e.target.value)}
-            value={email}
-            required
-          />
-          <p className="texto">Contraseña</p>
-          <input
-            placeholder="Contraseña"
-            type="password"
-            className="loginInput"
-            onChange={(e) => handlePasswordChange(e.target.value)}
-            value={password}
-            required
-            minLength="8"
-          />
-          {status !== "Login exitoso." && (
-            <p className="textoError">{status}</p>
-          )}
-          <button className="loginButton">Ingresar</button>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div className="login">
+            <div className="loginWrapper">
+                <img className="BackArrow" src={BackArrow} onClick={GoBack} alt="tilin"></img>
+                <img src={logo} alt="Logo testHero" />
+                <form className="loginBox" onSubmit={handleLogin}>
+                    <p className="texto">Correo</p>
+                    <input
+                        placeholder="Correo"
+                        type="email"
+                        className="loginInput"
+                        onChange={(e) => handleEmailChange(e.target.value)}
+                        value={email}
+                        required
+                    />
+                    <p className="texto">Contraseña</p>
+                    <input
+                        placeholder="Contraseña"
+                        type="password"
+                        className="loginInput"
+                        onChange={(e) => handlePasswordChange(e.target.value)}
+                        value={password}
+                        required
+                        minLength="8"
+                    />
+                    {status !== "Login exitoso." && (
+                        <p className="textoError">{status}</p>
+                    )}
+                    <button className="loginButton">Ingresar</button>
+                </form>
+            </div>
+        </div>
+    );
 }
--- TestHero/ClientApp/src/Pages/loginalumno/LoginAlumno.js
+++ TestHero/ClientApp/src/Pages/loginalumno/LoginAlumno.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./loginalumno.css";
 import logo from "../../assets/logo.png";
+import BackArrow from "../../assets/BackArrow.png"
 import { useState } from "react";
 import axios from "axios";
 import { useEffect, useContext } from "react";
@@ -8,103 +9,109 @@ import { useNavigate } from "react-router-dom";
 import ProfesorContext from "context/contextoProfesor";
 
 /**
- * @author Bernardo de la Sierra
- * @version 1.0.0
+ * @author Bernardo de la Sierra y Leonardo García
+ * @version 1.0.2
  * @license Gp
  * @params Sin parametros
  * @description Clase para que el usuario inicie sesion
  */
 
 export default function Login() {
-  // Parametros a actualizar
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [status, setStatus] = useState("");
-  const { state, setState } = useContext(ProfesorContext);
-  const navigate = useNavigate();
-  /**Cambia el correo */
-  const handleEmailChange = (value) => {
-    setEmail(value);
-  };
-  /**Cambia ela contraseña */
-  const handlePasswordChange = (value) => {
-    setPassword(value);
-  };
-  /** Hace el inicar sesion */
-  const handleLogin = async (e) => {
-    e.preventDefault();
+    // Parametros a actualizar
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [status, setStatus] = useState("");
+    const { state, setState } = useContext(ProfesorContext);
+    const navigate = useNavigate();
+    /**Cambia el correo */
+    const handleEmailChange = (value) => {
+        setEmail(value);
+    };
+    /**Cambia ela contraseña */
+    const handlePasswordChange = (value) => {
+        setPassword(value);
+    };
+    /** Hace el inicar sesion */
+    const handleLogin = async (e) => {
+        e.preventDefault();
 
-    const data = {
-      Correo: email,
-      Password: password,
+        const data = {
+            Correo: email,
+            Password: password,
+        };
+
+        const url = "api/login/alumno";
+
+        try {
+            const result = await axios.post(url, data);
+            setStatus(result.data.message);
+            if (result.data.message === "Login exitoso") {
+                getAlumno(result.data.id);
+            }
+        } catch (error) {
+            alert(error);
+        }
+    };
+    /**Checa que dado un idAlumno nos guarde su informacion */
+    const getAlumno = async (id) => {
+        const url = `api/alumno/${id}`;
+
+        try {
+            const result = await axios.get(url);
+            let res = result.data[0];
+            let alumno = {
+                id: res.idAlumno,
+                nombre: res.nombres + " " + res.apellidos,
+                correo: res.correo,
+            };
+            setState(alumno);
+        } catch (error) {
+            alert(error);
+        }
     };
 
-    const url = "api/login/alumno";
+    useEffect(() => {
+        if (status === "Login exitoso") {
+            navigate("/homealumno");
+        }
+    }, [status]);
 
-    try {
-      const result = await axios.post(url, data);
-      setStatus(result.data.message);
-      if (result.data.message === "Login exitoso") {
-        getAlumno(result.data.id);
-      }
-    } catch (error) {
-      alert(error);
-    }
-  };
-  /**Checa que dado un idAlumno nos guarde su informacion */
-  const getAlumno = async (id) => {
-    const url = `api/alumno/${id}`;
+    // función para hacer el boton de regreso
+    function GoBack() {
+        navigate("/");
+    };
 
-    try {
-      const result = await axios.get(url);
-      let res = result.data[0];
-      let alumno = {
-        id: res.idAlumno,
-        nombre: res.nombres + " " + res.apellidos,
-        correo: res.correo,
-      };
-      setState(alumno);
-    } catch (error) {
-      alert(error);
-    }
-  };
-
-  useEffect(() => {
-    if (status === "Login exitoso") {
-      navigate("/homealumno");
-    }
-  }, [status]);
-
-  return (
-    <div className="login">
-      <div className="loginWrapper">
-        <img src={logo} alt="Logo testHero" />
-        <form className="loginBox" onSubmit={handleLogin}>
-          <p className="texto">Correo</p>
-          <input
-            placeholder="Correo"
-            type="email"
-            className="loginInput"
-            onChange={(e) => handleEmailChange(e.target.value)}
-            value={email}
-            required
-          />
-          <p className="texto">Contraseña</p>
-          <input
-            placeholder="Contraseña"
-            type="password"
-            className="loginInput"
-            onChange={(e) => handlePasswordChange(e.target.value)}
-            value={password}
-            required
-            minLength="8"
-          />
-          {status !== "Login exitoso." && (
-            <p className="textoError">{status}</p>
-          )}
-          <button className="loginButton">Ingresar</button>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div className="login">
+            <div className="loginWrapper">
+                <img className="BackArrow" src={BackArrow} onClick={GoBack} alt="tilin"></img>
+                <img src={logo} alt="Logo testHero" />
+                <form className="loginBox" onSubmit={handleLogin}>
+                    <p className="texto">Correo</p>
+                    <input
+                        placeholder="Correo"
+                        type="email"
+                        className="loginInput"
+                        onChange={(e) => handleEmailChange(e.target.value)}
+                        value={email}
+                        required
+                    />
+                    <p className="texto">Contraseña</p>
+                    <input
+                        placeholder="Contraseña"
+                        type="password"
+                        className="loginInput"
+                        onChange={(e) => handlePasswordChange(e.target.value)}
+                        value={password}
+                        required
+                        minLength="8"
+                    />
+                    {status !== "Login exitoso." && (
+                        <p className="textoError">{status}</p>
+                    )}
+                    <button className="loginButton">Ingresar</button>
+                </form>
+            </div>
+        </div>
+    );
 }
