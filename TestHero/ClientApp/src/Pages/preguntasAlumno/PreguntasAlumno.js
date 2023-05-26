a1bb5ad19ea5b7a7eb6028afae700428b0ad98e4
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-05-26 11:56:06 -0600

    Correciones 2

--- TestHero/ClientApp/src/Pages/preguntasAlumno/PreguntasAlumno.js
+++ TestHero/ClientApp/src/Pages/preguntasAlumno/PreguntasAlumno.js
@@ -72,6 +72,7 @@ export default function PreguntasAlumno() {
             {preguntas && alumnoRespuesta.length === 0 && (
               <>
                 <div className="vacio">
+                  <br />
                   Todavia no has resuelto un examen para ver tus respuestas
                 </div>
               </>
